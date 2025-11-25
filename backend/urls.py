22895c4f4f2bc0a81192c810036178fd4d2a29b5
RG <merajagopalsrm@gmail.com>
2025-11-25 15:28:50 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "Django backend is running!"})
 
 urlpatterns = [
+    path('', home),                  # <-- ADD THIS
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
