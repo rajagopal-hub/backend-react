87ae300b39258f4b67bcb3669bf647968afd34e3
RG <merajagopalsrm@gmail.com>
2025-11-26 11:35:51 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.urls import path
+from . import views
 
 def home(request):
     return JsonResponse({"status": "Django backend is running!"})
 
 urlpatterns = [
-    path('', home),                  # <-- ADD THIS
+    path('api/', views.api_home),                  # <-- ADD THIS
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
